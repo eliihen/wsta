f723ca1a71a00aba2080a20d9357a04148dbf97b
Espen Henriksen <dev@henriksen.is>
2016-05-01 20:33:26 +0200

    Add more command line arguments to parser

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,55 @@ extern crate argparse;
 mod program;
 pub mod options;
 
-use argparse::{ArgumentParser, Store, StoreTrue, Print};
+use std::vec::Vec;
+
+use argparse::{ArgumentParser, Store, StoreTrue, Print, Collect, IncrBy};
 
 use options::Options;
 
-/// Parse command line options and invoke the next method
+/// Parse command line options and invoke program
 fn main() {
 
+    // Implement default options
     let mut options = Options {
-        url: String::from(""),
+        url: String::new(),
+        login_url: String::new(),
         quiet: false,
+        verbosity: 0,
+        print_headers: false,
+        headers: Vec::new()
     };
 
     {  // this block limits scope of borrows by ap.refer() method
         let mut ap = ArgumentParser::new();
 
         // TODO NO WORK ap.set_description(env!("CARGO_PKG_DESCRIPTION"));
-        ap.set_description("The WebSocket Transfer Agent.");
+        ap.set_description("The WebSocket Transfer Agent");
 
         ap.refer(&mut options.url)
             .required()
             .add_option(&["-u", "--url"], Store,
                         "URL of the server to connect with");
 
+        ap.refer(&mut options.login_url)
+            .add_option(&["-l", "--login"], Store,
+                        "(TODO) URL to authenticate with before connecting to WS");
+
+        ap.refer(&mut options.headers)
+            .add_option(&["-h", "--header"], Collect,
+                        "(TODO) add headers to any HTTP request made");
+
+        ap.refer(&mut options.print_headers)
+            .add_option(&["-I", "--head"], StoreTrue,
+                        "(TODO) print HTTP headers");
+
         ap.refer(&mut options.quiet)
             .add_option(&["-q", "--quiet"], StoreTrue,
                         "only output incoming frames without any decoration");
+
+        ap.refer(&mut options.verbosity)
+            .add_option(&["-v", "--verbose"], IncrBy(1),
+                        "(TODO) increase the verbosity level by one");
 
         ap.add_option(&["-V", "--version"],
                       Print(format!("{} {}",
--- src/options.rs
+++ src/options.rs
@@ -1,6 +1,29 @@
 /// The command line options provided to the program
 pub struct Options {
+
+    /// Flag the application to be more pipe-friendly.
+    /// Only print the incoming websocket frames.
+    pub quiet: bool,
+
+    /// The verbosity level of the application. Should be a number
+    /// between 0 and 3.
+    ///   0: NO LOGGING
+    ///   1: EVENT LOGGING
+    ///   2: DEBUG LOGGING
+    /// >=3: TRACE LOGGING
+    pub verbosity: u8,
+
+    /// The WebSocket URL to connect to.
     pub url: String,
-    pub quiet: bool
+
+    /// Optional: A GET URL to authenticate with before connecting
+    /// to the main url.
+    pub login_url: String,
+
+    /// Print the headers of any HTTP request when true.
+    pub print_headers: bool,
+
+    /// Headers
+    pub headers: Vec<String>
 }
 
