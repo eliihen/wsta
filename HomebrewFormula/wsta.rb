0079c1d81d80d4ee32119c490c740193b3905bdf
Espen Henriksen <dev@henriksen.is>
2016-05-09 19:50:59 +0200

    Make homebrew formula work

--- HomebrewFormula/wsta.rb
+++ HomebrewFormula/wsta.rb
@@ -2,17 +2,22 @@ class Wsta < Formula
   desc "A cli tool written in rust for interfacing with WebSocket services."
   homepage "https://github.com/esphen/wsta"
   url "https://github.com/esphen/wsta/archive/0.2.0.tar.gz"
-  sha256 "98af296907b371083289a8b35bd6ff24cfd8fafb013033b724aacb5fe774c9b1"
+  sha256 "205a90215f5413a520f7d6ba7507c66e4273796751a6d8053ce9f2216127f1d9"
 
-  depends_on 'pointlessone/homebrew-rust-nightly/rust-nightly' => :build
-  depends_on 'pointlessone/homebrew-rust-nightly/cargo-nightly' => :build
+  depends_on 'gpg' => :build
+  depends_on 'multirust' => :build
   depends_on 'openssl'
 
   def install
+    system "multirust", "update", "beta"
+    system "multirust", "override", "beta"
     system "cargo", "build", "--release"
-    bin.install Dir["target", "release", "wsta"]
+
+    bin.mkpath
+    bin.install "target/release/wsta"
 
     man.mkpath
     man1.install "wsta.1"
   end
 end
+
