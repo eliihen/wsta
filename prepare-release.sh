506d6c537ea9df06cd294663796deed27413a6c0
Espen Henriksen <dev@henriksen.is>
2016-05-17 17:35:48 +0200

    Fix wsta.dsc version check and shellcheck

--- prepare-release.sh
+++ prepare-release.sh
@@ -4,15 +4,15 @@
 
 check_version_numbers() {
 
-  files=(Cargo.toml build/wsta.spec HomebrewFormula/wsta.rb build/debian/wsta.dsc build/debian/changelog wsta.1)
+  files=(Cargo.toml build/wsta.spec HomebrewFormula/wsta.rb build/wsta.dsc build/debian/changelog wsta.1)
 
   for file in "${files[@]}"; do
-    echo $file $1
-    grep -q $1 $file;
+    echo "$file" "$1"
+    grep -q "$1" "$file"
     status=$?
     if [ $status -ne 0 ]; then
       echo ----------------------------------------------
-      echo remember to set version $1 in $file
+      echo remember to set version "$1" in "$file"
       echo ----------------------------------------------
     fi
   done
@@ -23,11 +23,11 @@ if [ "$1" == "" ]; then
   exit 1
 fi
 
-check_version_numbers $1
+check_version_numbers "$1"
 
 echo Cleaning up after you....
 rm -rfv deploy
-rm -rf ~/.cargo
+rm -rf ~/.cargo/registry
 cargo clean
 
 ./package-debian.sh
@@ -36,14 +36,15 @@ echo Fetching offline assets
 cargo fetch
 
 echo Creating tarball
-mkdir -p deploy/wsta-$1
-cp -rv {Cargo.*,src,wsta.1,README.md,Makefile,LICENCE} deploy/wsta-$1
-cp -r ~/.cargo deploy/wsta-$1
-cp -v rust/* deploy/wsta-$1
-tar -C deploy -czf deploy/$1.tar.gz wsta-$1
-cp deploy/$1.tar.gz deploy/wsta_$1.orig.tar.gz
+mkdir -p "deploy/wsta-$1"
+cp -rv {Cargo.*,src,wsta.1,README.md,Makefile,LICENCE} "deploy/wsta-$1"
+mkdir -v  "deploy/wsta-$1/.cargo"
+cp -r ~/.cargo/registry "deploy/wsta-$1/.cargo"
+cp -v rust/* "deploy/wsta-$1"
+tar -C deploy -czf "deploy/$1.tar.gz" "wsta-$1"
+cp "deploy/$1.tar.gz" "deploy/wsta_$1.orig.tar.gz"
 
 
-rm -rf deploy/wsta-$1
+rm -rf "deploy/wsta-$1"
 
 echo Done!
