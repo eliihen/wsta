968a89ac2b87a3c71ae04332fc48e529d96f9bc6
Espen Henriksen <dev@henriksen.is>
2016-05-31 22:56:27 +0200

    Update sha sum of 0.2.1

--- HomebrewFormula/wsta.rb
+++ HomebrewFormula/wsta.rb
@@ -2,7 +2,7 @@ class Wsta < Formula
   desc "A cli tool written in rust for interfacing with WebSocket services."
   homepage "https://github.com/esphen/wsta"
   url "https://github.com/esphen/wsta/archive/0.2.1.tar.gz"
-  sha256 "7923e9bd8310b5a72d8390324bd5150615d0e587ec793f808e12cddbb03e238f"
+  sha256 "48c2c1a73cab9955df0c2cb494d536e904dafa19a7c8ac7c6dac64ae2cb6240a"
 
   depends_on 'gpg' => :build
   depends_on 'multirust' => :build
