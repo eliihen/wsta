71b92ae6aa917b122235f79c3ea1639a1cea92de
Espen Henriksen <dev@henriksen.is>
2016-10-01 16:21:03 +0200

    Release 0.4.1 Homebrew formula

--- HomebrewFormula/wsta.rb
+++ HomebrewFormula/wsta.rb
@@ -1,8 +1,8 @@
 class Wsta < Formula
   desc "A cli tool written in rust for interfacing with WebSocket services."
   homepage "https://github.com/esphen/wsta"
-  url "https://github.com/esphen/wsta/archive/0.4.0.tar.gz"
-  sha256 "dbb5c5900d595254c73b485493a96618ee5d12402e17d1aa3a57dcdb5cac3b5d"
+  url "https://github.com/esphen/wsta/archive/0.4.1.tar.gz"
+  sha256 "0c031dbf490c98dbc5dab07f16945ba353cd3cd18780094aee17f4782ec0ea57"
 
   depends_on 'gpg' => :build
   depends_on 'rust' => :build
